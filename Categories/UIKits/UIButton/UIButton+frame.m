dfe6bc27b48f90d43a325cee501c1fd7049b0286
zhengqidian <zhengqidian@souche.com>
2016-10-17 10:25:46 +0800

    删除不必要代码

--- Categories/UIKits/UIButton/UIButton+frame.m
+++ Categories/UIKits/UIButton/UIButton+frame.m
@@ -50,35 +50,6 @@
 }
 
 
--(float)titleCenter{
-    
-    return self.frame.size.width/2;
-}
--(void)setTitleCenter{
-    
-    if (self.contentHorizontalAlignment==UIControlContentHorizontalAlignmentLeft) {
-        
-        //相对于初始位置的偏移,AlignmentLeft时title初始位置永远为image的右边位置(当title改变时马上执行此方法，titlelabel的frame还没修改，所以要延时处理)
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self setTitleEdgeInsets:UIEdgeInsetsMake(0, (self.frame.size.width-self.titleLabel.frame.size.width)/2-self.imageView.frame.size.width, 0, 0)];
-        });
-        
-    }else if (self.contentHorizontalAlignment==UIControlContentHorizontalAlignmentCenter){
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self setTitleEdgeInsets:UIEdgeInsetsMake(0, (self.imageView.frame.size.width)/2-self.imageView.frame.size.width, 0, -(self.imageView.frame.size.width)/2+self.imageView.frame.size.width)];
-        });
-        
-    }else if (self.contentHorizontalAlignment==UIControlContentHorizontalAlignmentRight){
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self setTitleEdgeInsets:UIEdgeInsetsMake(0, 0, 0, -(self.frame.size.width-self.titleLabel.frame.size.width)/2+self.frame.size.width-self.titleLabel.frame.size.width)];
-        });
-    }
-    
-}
-
-
 -(void)setImageToTitleRight{
     
     [self setImageToTitleRightWithPadding:0];
